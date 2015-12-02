57d219f596c10d7e1fd4cf211bf92c21624fd35f
rikkertkoppes <rikkert@rikkertkoppes.com>
2015-12-02 21:41:08 +0100

    Started with matrix transformations to get positioning right. Made anchor the middle of blocks.

--- Blocks/Build.js
+++ Blocks/Build.js
@@ -14,7 +14,7 @@ var MenuElement = (function () {
             var svg = document.createElementNS(SVGNS, 'svg');
             svg.setAttribute('width', '50');
             svg.setAttribute('height', '50');
-            svg.setAttribute('viewBox', '0 0 100 100');
+            svg.setAttribute('viewBox', '-525 -525 1050 1050');
             svg.appendChild(this.renderPath());
             d.appendChild(svg);
         }
@@ -52,27 +52,40 @@ var Canvas = (function () {
     function Canvas(id) {
         this.el = document.getElementById(id);
         this.svg = this.el.appendChild(document.createElementNS(SVGNS, 'svg'));
+        this.g = this.svg.appendChild(document.createElementNS(SVGNS, 'g'));
+        this.g.setAttribute('transform', 'scale(0.1)');
         this.dragHandler = this.drag.bind(this);
         this.dragEndHandler = this.endDrag.bind(this);
     }
     Canvas.prototype.drawItem = function (item) {
         var path = item.renderPath();
         path.addEventListener('mousedown', this.startDrag.bind(this, path));
-        this.svg.appendChild(path);
+        this.g.appendChild(path);
         var bb = path.getBBox();
         console.log(bb);
     };
+    Canvas.prototype.getMousePoint = function (e) {
+        var pt = this.svg.createSVGPoint();
+        pt.x = e.pageX;
+        pt.y = e.pageY;
+        console.log(pt, this.g.getScreenCTM());
+        return pt.matrixTransform(this.g.getScreenCTM().inverse());
+    };
     Canvas.prototype.startDrag = function (path, e) {
         this.dragPath = path;
-        this.anchor = [e.pageX, e.pageY];
+        this.anchor = this.getMousePoint(e);
         window.addEventListener('mousemove', this.dragHandler);
         window.addEventListener('mouseup', this.dragEndHandler);
         console.log('start', this.anchor);
     };
     Canvas.prototype.drag = function (e) {
-        var d = [e.pageX - this.anchor[0], e.pageY - this.anchor[1]];
+        var point = this.getMousePoint(e);
+        var d = [point.x - this.anchor.x, point.y - this.anchor.y];
         var t = this.dragPath.transform;
-        console.log(t);
+        var m = this.dragPath.getCTM();
+        console.log(m);
+        this.dragPath.setAttribute('transform', 'translate(' + d.join(',') + ')');
+        // console.log(t);
         console.log(d);
     };
     Canvas.prototype.endDrag = function (e) {
@@ -83,14 +96,15 @@ var Canvas = (function () {
 })();
 var c = new Canvas('canvas');
 var m = new Menu('menu', c)
-    .addElement('hrect', 'M 0 25 L 100 25 L 100 75 L 0 75 Z')
-    .addElement('vrect', 'M 25 0 L 75 0 L 75 100 L 25 100 Z')
-    .addElement('hplate', 'M 0 50 L 100 50 L 100 75 L 0 75 Z')
-    .addElement('vplate', 'M 25 0 L 50 0 L 50 100 L 25 100 Z')
-    .addElement('ssquare', 'M 25 25 L 25 75 L 75 75 L 75 25 Z')
-    .addElement('lsquare', 'M 0 0 L 0 100 L 100 100 L 100 0 Z')
-    .addElement('arc', 'M 0 25 L 100 25 L 100 75 L 75 75 A 25 25 0 1 0 25 75 L 0 75 Z')
-    .addElement('cylinder', 'M 25 50 A 25 25 0 1 0 50 25 A 25 25 0 0 0 25 50 Z')
-    .addElement('ltriangle', 'M 25 75 L 75 75 L 50 50 Z')
-    .addElement('ltriangle', 'M 0 75 L 100 75 L 50 25 Z');
+    .addElement('hrect', 'M -512 -256 L -512 256 L 512 256 L 512 -256 Z')
+    .addElement('vrect', 'M -256 -512 L 256 -512 L 256 512 L -256 512 Z')
+    .addElement('hplate', 'M -512 -128 L -512 128 L 512 128 L 512 -128 Z')
+    .addElement('vplate', 'M -128 -512 L 128 -512 L 128 512 L -128 512 Z')
+    .addElement('ssquare', 'M -256 -256 L -256 256 L 256 256 L 256 -256 Z')
+    .addElement('lsquare', 'M -512 -512 L -512 512 L 512 512 L 512 -512 Z')
+    .addElement('arc', 'M -512 -256 L 512 -256 L 512 256 L 256 256 A 256 256 0 1 0 -256 256 L -512 256 Z')
+    .addElement('cylinder', 'M -256 0 A 256 256 0 1 0 0 -256 A 256 256 0 0 0 -256 0 Z')
+    .addElement('striangle', 'M -256 128 L 256 128 L 0 -128 Z')
+    .addElement('ltriangle', 'M -512 256 L 512 256 L 0 -256 Z');
 console.log(c, m);
+//# sourceMappingURL=Build.js.map
--- Blocks/Build.ts
+++ Blocks/Build.ts
@@ -18,7 +18,7 @@ class MenuElement {
             var svg = <SVGElement> document.createElementNS(SVGNS, 'svg');
             svg.setAttribute('width', '50');
             svg.setAttribute('height', '50');
-            svg.setAttribute('viewBox', '0 0 100 100');
+            svg.setAttribute('viewBox', '-525 -525 1050 1050');
             svg.appendChild(this.renderPath());
             d.appendChild(svg);
         }
@@ -58,35 +58,50 @@ class Menu {
 
 class Canvas {
     el: HTMLElement;
-    svg: SVGElement;
-    anchor: number[];
+    svg: SVGSVGElement;
+    g: SVGGElement;
+    anchor: SVGPoint;
     dragHandler: (e: MouseEvent) => void;
     dragEndHandler: (e: MouseEvent) => void;
     dragPath: SVGPathElement;
     constructor(id:string) {
         this.el = document.getElementById(id);
-        this.svg = <SVGElement> this.el.appendChild(document.createElementNS(SVGNS, 'svg'));
+        this.svg = <SVGSVGElement> this.el.appendChild(document.createElementNS(SVGNS, 'svg'));
+        this.g = <SVGGElement> this.svg.appendChild(document.createElementNS(SVGNS, 'g'));
+        this.g.setAttribute('transform', 'scale(0.1)');
         this.dragHandler = this.drag.bind(this);
         this.dragEndHandler = this.endDrag.bind(this);
     }
     drawItem(item:MenuElement) {
         var path = item.renderPath();
         path.addEventListener('mousedown', this.startDrag.bind(this, path));
-        this.svg.appendChild(path);
+        this.g.appendChild(path);
         var bb = path.getBBox();
         console.log(bb);
     }
+    getMousePoint(e) {
+        var pt = this.svg.createSVGPoint();
+        pt.x = e.pageX; pt.y = e.pageY;
+        console.log(pt,this.g.getScreenCTM())
+        return pt.matrixTransform(this.g.getScreenCTM().inverse());
+    }
     startDrag(path:SVGPathElement, e:MouseEvent) {
         this.dragPath = path;
-        this.anchor = [e.pageX, e.pageY];
+        this.anchor = this.getMousePoint(e);
         window.addEventListener('mousemove', this.dragHandler);
         window.addEventListener('mouseup', this.dragEndHandler);
         console.log('start', this.anchor);
     }
     drag(e:MouseEvent) {
-        var d = [e.pageX - this.anchor[0], e.pageY - this.anchor[1]];
+        var point = this.getMousePoint(e);
+        var d = [point.x - this.anchor.x, point.y - this.anchor.y];
         var t = this.dragPath.transform;
-        console.log(t);
+        var m = this.dragPath.getCTM();
+        console.log(m);
+        m.translate.apply(m, d);
+        console.log(m);
+        this.dragPath.setAttribute('transform', 'translate(' + d.join(',') + ')');
+        // console.log(t);
         console.log(d);
     }
     endDrag(e:MouseEvent) {
@@ -98,15 +113,15 @@ class Canvas {
 
 var c = new Canvas('canvas');
 var m = new Menu('menu',c)
-    .addElement('hrect','M 0 25 L 100 25 L 100 75 L 0 75 Z')
-    .addElement('vrect','M 25 0 L 75 0 L 75 100 L 25 100 Z')
-    .addElement('hplate', 'M 0 50 L 100 50 L 100 75 L 0 75 Z')
-    .addElement('vplate', 'M 25 0 L 50 0 L 50 100 L 25 100 Z')
-    .addElement('ssquare','M 25 25 L 25 75 L 75 75 L 75 25 Z')
-    .addElement('lsquare','M 0 0 L 0 100 L 100 100 L 100 0 Z')
-    .addElement('arc','M 0 25 L 100 25 L 100 75 L 75 75 A 25 25 0 1 0 25 75 L 0 75 Z')
-    .addElement('cylinder','M 25 50 A 25 25 0 1 0 50 25 A 25 25 0 0 0 25 50 Z')
-    .addElement('ltriangle','M 25 75 L 75 75 L 50 50 Z')
-    .addElement('ltriangle','M 0 75 L 100 75 L 50 25 Z');
+    .addElement('hrect','M -512 -256 L -512 256 L 512 256 L 512 -256 Z')
+    .addElement('vrect','M -256 -512 L 256 -512 L 256 512 L -256 512 Z')
+    .addElement('hplate', 'M -512 -128 L -512 128 L 512 128 L 512 -128 Z')
+    .addElement('vplate', 'M -128 -512 L 128 -512 L 128 512 L -128 512 Z')
+    .addElement('ssquare','M -256 -256 L -256 256 L 256 256 L 256 -256 Z')
+    .addElement('lsquare','M -512 -512 L -512 512 L 512 512 L 512 -512 Z')
+    .addElement('arc','M -512 -256 L 512 -256 L 512 256 L 256 256 A 256 256 0 1 0 -256 256 L -512 256 Z')
+    .addElement('cylinder','M -256 0 A 256 256 0 1 0 0 -256 A 256 256 0 0 0 -256 0 Z')
+    .addElement('striangle','M -256 128 L 256 128 L 0 -128 Z')
+    .addElement('ltriangle','M -512 256 L 512 256 L 0 -256 Z');
 
 console.log(c,m);
