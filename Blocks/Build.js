7dcba7727c359df5e40d1eb20b1c9fe75c94508f
rikkertkoppes <rikkert@rikkertkoppes.com>
2015-12-03 19:26:09 +0100

    Added methods to stack items and create a network from the structure.

--- Blocks/Build.js
+++ Blocks/Build.js
@@ -24,6 +24,7 @@ var MenuElement = (function () {
     MenuElement.prototype.renderPath = function () {
         var p = document.createElementNS(SVGNS, 'path');
         p.setAttribute('d', this.path);
+        p.setAttribute('class', this.name);
         p.setAttribute('transform', 'matrix(1 0 0 1 0 0)');
         return p;
     };
@@ -51,19 +52,24 @@ var Menu = (function () {
 })();
 var Canvas = (function () {
     function Canvas(id) {
+        this.paths = [];
         this.el = document.getElementById(id);
         this.svg = this.el.appendChild(document.createElementNS(SVGNS, 'svg'));
         this.g = this.svg.appendChild(document.createElementNS(SVGNS, 'g'));
         this.g.setAttribute('transform', 'scale(0.1)');
-        this.dragHandler = this.drag.bind(this);
-        this.dragEndHandler = this.endDrag.bind(this);
+        this.dragMoveHandler = this.dragMove.bind(this);
+        this.dragEndHandler = this.dragEnd.bind(this);
     }
     Canvas.prototype.drawItem = function (item) {
         var path = item.renderPath();
         path.setAttribute('transform', 'matrix(1 0 0 1 1000 1000)');
-        path.addEventListener('mousedown', this.startDrag.bind(this, path));
+        path.addEventListener('mousedown', this.dragStart.bind(this, path));
+        this.paths.push(path);
         this.g.appendChild(path);
-        var bb = path.getBBox();
+        this.drop(path);
+        if (this.onUpdate) {
+            this.onUpdate(this);
+        }
     };
     Canvas.prototype.getMousePoint = function (e) {
         var pt = this.svg.createSVGPoint();
@@ -71,22 +77,70 @@ var Canvas = (function () {
         pt.y = e.pageY;
         return pt.matrixTransform(this.g.getScreenCTM().inverse());
     };
-    Canvas.prototype.startDrag = function (path, e) {
+    Canvas.prototype.getEdges = function (path) {
+        var m = path.transform.baseVal[0].matrix;
+        var bb = path.getBBox();
+        return {
+            left: m.e - bb.width / 2,
+            right: m.e + bb.width / 2,
+            top: m.f - bb.height / 2,
+            bottom: m.f + bb.height / 2
+        };
+    };
+    Canvas.prototype.supports = function (query, base) {
+        var qlr = this.getEdges(query);
+        var blr = this.getEdges(base);
+        return ((query !== base) &&
+            (qlr.right > blr.left) &&
+            (qlr.left < blr.right) &&
+            (qlr.bottom === blr.top));
+    };
+    Canvas.prototype.touches = function (query, base) {
+        var qlr = this.getEdges(query);
+        var blr = this.getEdges(base);
+        return ((query !== base) &&
+            ((qlr.bottom > blr.top) ||
+                (qlr.top < blr.bottom)) &&
+            ((Math.abs(qlr.right - blr.left) < 50) ||
+                (Math.abs(qlr.left - blr.right) < 50)));
+    };
+    Canvas.prototype.drop = function (path) {
+        var _this = this;
+        var lr = this.getEdges(path);
+        //get heighest level within the width of the dropped item
+        var floor = this.paths.reduce(function (floor, item) {
+            var ilr = _this.getEdges(item);
+            if ((item !== path) && (lr.right > ilr.left) && (lr.left < ilr.right)) {
+                var ptop = ilr.top;
+                return Math.min(ptop, floor);
+            }
+            return floor;
+        }, 3000);
+        var bb = path.getBBox();
+        var m = path.transform.baseVal[0].matrix;
+        var y = floor - bb.height / 2;
+        path.setAttribute('transform', 'translate(' + m.e + ',' + y + ')');
+    };
+    Canvas.prototype.dragStart = function (path, e) {
         this.dragPath = path;
         this.anchor = this.getMousePoint(e);
         this.anchorMatrix = path.transform.baseVal[0].matrix;
-        window.addEventListener('mousemove', this.dragHandler);
+        window.addEventListener('mousemove', this.dragMoveHandler);
         window.addEventListener('mouseup', this.dragEndHandler);
     };
-    Canvas.prototype.drag = function (e) {
+    Canvas.prototype.dragMove = function (e) {
         var point = this.getMousePoint(e);
         var d = [point.x - this.anchor.x, point.y - this.anchor.y];
         var m = this.anchorMatrix.translate.apply(this.anchorMatrix, d);
         this.dragPath.setAttribute('transform', 'translate(' + m.e + ',' + m.f + ')');
     };
-    Canvas.prototype.endDrag = function (e) {
-        window.removeEventListener('mousemove', this.dragHandler);
+    Canvas.prototype.dragEnd = function (e) {
+        window.removeEventListener('mousemove', this.dragMoveHandler);
         window.removeEventListener('mouseup', this.dragEndHandler);
+        this.drop(this.dragPath);
+        if (this.onUpdate) {
+            this.onUpdate(this);
+        }
     };
     return Canvas;
 })();
@@ -103,4 +157,51 @@ var m = new Menu('menu', c)
     .addElement('striangle', 'M -256 128 L 256 128 L 0 -128 Z')
     .addElement('ltriangle', 'M -512 256 L 512 256 L 0 -256 Z');
 console.log(c, m);
+var Network = (function () {
+    function Network(id) {
+        this.net = new vis.Network(document.getElementById(id), {}, {});
+    }
+    Network.prototype.fromCanvas = function (canvas) {
+        this.canvas = canvas;
+        canvas.onUpdate = this.update.bind(this);
+    };
+    Network.prototype.getRelations = function (path, i) {
+        var _this = this;
+        return this.canvas.paths.reduce(function (relations, item, j) {
+            if (_this.canvas.supports(path, item)) {
+                relations.push({
+                    from: j,
+                    to: i,
+                    label: 'supports',
+                    arrows: 'to'
+                });
+            }
+            else if (_this.canvas.touches(path, item)) {
+                relations.push({
+                    from: j,
+                    to: i,
+                    label: 'touches',
+                    arrows: 'to'
+                });
+            }
+            return relations;
+        }, []);
+    };
+    Network.prototype.getNode = function (path, i) {
+        return {
+            id: i,
+            label: path.getAttribute('class')
+        };
+    };
+    Network.prototype.update = function (canvas) {
+        var _this = this;
+        var nodes = canvas.paths.map(this.getNode);
+        var edges = canvas.paths.reduce(function (all, path, i) {
+            return all.concat(_this.getRelations(path, i));
+        }, []);
+        this.net.setData({ nodes: nodes, edges: edges });
+    };
+    return Network;
+})();
+var n = new Network('network').fromCanvas(c);
 //# sourceMappingURL=Build.js.map
--- Blocks/Build.ts
+++ Blocks/Build.ts
@@ -28,6 +28,7 @@ class MenuElement {
     renderPath(): SVGPathElement {
         var p = <SVGPathElement> document.createElementNS(SVGNS, 'path');
         p.setAttribute('d', this.path);
+        p.setAttribute('class', this.name);
         p.setAttribute('transform', 'matrix(1 0 0 1 0 0)');
         return p;
     }
@@ -63,46 +64,108 @@ class Canvas {
     g: SVGGElement;
     anchor: SVGPoint;
     anchorMatrix: SVGMatrix;
-    dragHandler: (e: MouseEvent) => void;
+    paths: SVGPathElement[];
+    dragMoveHandler: (e: MouseEvent) => void;
     dragEndHandler: (e: MouseEvent) => void;
     dragPath: SVGPathElement;
+    onUpdate: (canvas:Canvas) => void;
     constructor(id:string) {
+        this.paths = [];
         this.el = document.getElementById(id);
         this.svg = <SVGSVGElement> this.el.appendChild(document.createElementNS(SVGNS, 'svg'));
         this.g = <SVGGElement> this.svg.appendChild(document.createElementNS(SVGNS, 'g'));
         this.g.setAttribute('transform', 'scale(0.1)');
-        this.dragHandler = this.drag.bind(this);
-        this.dragEndHandler = this.endDrag.bind(this);
+        this.dragMoveHandler = this.dragMove.bind(this);
+        this.dragEndHandler = this.dragEnd.bind(this);
     }
-    drawItem(item:MenuElement) {
+    drawItem(item:MenuElement):void {
         var path = item.renderPath();
         path.setAttribute('transform', 'matrix(1 0 0 1 1000 1000)');
-        path.addEventListener('mousedown', this.startDrag.bind(this, path));
+        path.addEventListener('mousedown', this.dragStart.bind(this, path));
+        this.paths.push(path);
         this.g.appendChild(path);
-        var bb = path.getBBox();
+        this.drop(path);
+        if (this.onUpdate) {
+            this.onUpdate(this);
+        }
     }
-    getMousePoint(e) {
+    getMousePoint(e): SVGPoint {
         var pt = this.svg.createSVGPoint();
         pt.x = e.pageX; pt.y = e.pageY;
         return pt.matrixTransform(this.g.getScreenCTM().inverse());
     }
-    startDrag(path:SVGPathElement, e:MouseEvent) {
+    getEdges(path: SVGPathElement) {
+        var m = path.transform.baseVal[0].matrix;
+        var bb = path.getBBox();
+        return {
+            left: m.e - bb.width / 2,
+            right: m.e + bb.width / 2,
+            top: m.f - bb.height/2,
+            bottom: m.f + bb.height/2
+        };
+    }
+    supports(query:SVGPathElement,base:SVGPathElement) {
+        var qlr = this.getEdges(query);
+        var blr = this.getEdges(base);
+        return (
+            (query !== base) &&
+            (qlr.right > blr.left) &&
+            (qlr.left < blr.right) &&
+            (qlr.bottom === blr.top)
+        );
+    }
+    touches(query:SVGPathElement,base:SVGPathElement) {
+        var qlr = this.getEdges(query);
+        var blr = this.getEdges(base);
+        return (
+            (query !== base) &&
+            (
+                (qlr.bottom > blr.top) ||
+                (qlr.top < blr.bottom)
+            ) &&
+            (
+                (Math.abs(qlr.right - blr.left) < 50) || 
+                (Math.abs(qlr.left - blr.right) < 50)
+            )
+        );
+    }
+    drop(path: SVGPathElement):void {
+        var lr = this.getEdges(path);
+        //get heighest level within the width of the dropped item
+        var floor = this.paths.reduce((floor, item) => {
+            var ilr = this.getEdges(item);
+            if ((item !== path) && (lr.right > ilr.left) && (lr.left < ilr.right)) {
+                var ptop = ilr.top;
+                return Math.min(ptop, floor);
+            }
+            return floor;
+        },3000);
+        var bb = path.getBBox();
+        var m = path.transform.baseVal[0].matrix;
+        var y = floor - bb.height / 2;
+        path.setAttribute('transform', 'translate(' + m.e + ',' + y + ')');
+    }
+    dragStart(path:SVGPathElement, e:MouseEvent):void {
         this.dragPath = path;
         this.anchor = this.getMousePoint(e);
         this.anchorMatrix = path.transform.baseVal[0].matrix;
-        window.addEventListener('mousemove', this.dragHandler);
+        window.addEventListener('mousemove', this.dragMoveHandler);
         window.addEventListener('mouseup', this.dragEndHandler);
     }
-    drag(e:MouseEvent) {
+    dragMove(e:MouseEvent):void {
         var point = this.getMousePoint(e);
         var d = [point.x - this.anchor.x, point.y - this.anchor.y];
         var m = this.anchorMatrix.translate.apply(this.anchorMatrix,d);
 
         this.dragPath.setAttribute('transform', 'translate(' + m.e + ',' + m.f + ')');
     }
-    endDrag(e:MouseEvent) {
-        window.removeEventListener('mousemove', this.dragHandler);
+    dragEnd(e:MouseEvent):void {
+        window.removeEventListener('mousemove', this.dragMoveHandler);
         window.removeEventListener('mouseup', this.dragEndHandler);
+        this.drop(this.dragPath);
+        if (this.onUpdate) {
+            this.onUpdate(this);
+        }
     }
 }
 
@@ -121,3 +184,63 @@ var m = new Menu('menu',c)
     .addElement('ltriangle','M -512 256 L 512 256 L 0 -256 Z');
 
 console.log(c,m);
+
+//creating a network from the canvas and visualizing it
+//using http://visjs.org/ here
+declare var vis;
+
+interface visNode {
+    id: number;
+    label: string;
+}
+interface visEdge {
+    from: number;
+    to: number;
+}
+
+class Network {
+    net: any;
+    canvas: Canvas;
+    constructor(id:string) {
+        this.net = new vis.Network(document.getElementById(id), {}, {});
+    }
+    fromCanvas(canvas:Canvas) {
+        this.canvas = canvas;
+        canvas.onUpdate = this.update.bind(this);
+    }
+    getRelations(path:SVGPathElement,i): visEdge[] {
+        return this.canvas.paths.reduce((relations, item,j) => {
+            if (this.canvas.supports(path,item)) {
+                relations.push({
+                    from: j,
+                    to: i,
+                    label: 'supports',
+                    arrows: 'to'
+                });
+            } else if (this.canvas.touches(path, item)) {
+                relations.push({
+                    from: j,
+                    to: i,
+                    label: 'touches',
+                    arrows: 'to'
+                });
+            }
+            return relations;
+        }, []);
+    }
+    getNode(path: SVGPathElement, i: number):visNode {
+        return {
+            id: i,
+            label: path.getAttribute('class')
+        }
+    }
+    update(canvas) {
+        var nodes = canvas.paths.map(this.getNode);
+        var edges = canvas.paths.reduce((all, path, i) => {
+            return all.concat(this.getRelations(path, i));
+        },[]);
+        this.net.setData({ nodes: nodes, edges: edges });
+    }
+}
+
+var n = new Network('network').fromCanvas(c);
